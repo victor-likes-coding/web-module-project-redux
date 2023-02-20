7c0aa80e954cd4711947233c5732def29508cb95
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-20 18:23:03 -0500

    create store and use provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
 
-import reducer from './reducers';
+import reducer from "./reducers";
 
-import App from './components/App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>,
-  document.getElementById('root')
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
+    document.getElementById("root")
 );
