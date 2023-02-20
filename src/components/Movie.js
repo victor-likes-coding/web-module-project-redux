0709fbd80ebe66947724d4fe80b47e7af0b68099
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-20 18:57:27 -0500

    hook up delete functionality

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,50 +1,78 @@
-import React from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { useParams, useHistory } from "react-router-dom";
+import { deleteMovie } from "../actions/movieActions";
 
-const Movie = (props) => {
+const Movie = ({ movies, deleteMovie }) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const movies = [];
-    const movie = movies.find(movie=>movie.id===Number(id));
-    
-    return(<div className="modal-page col">
-        <div className="modal-dialog">
-            <div className="modal-content">
-                <div className="modal-header">						
-                    <h4 className="modal-title">{movie.title} Details</h4>
-                </div>
-                <div className="modal-body">
-                    <div className="flexContainer">
+    const movie = movies.find((movie) => movie.id === Number(id));
 
-                        <section className="movie-details">
-                            <div>
-                                <label>Title: <strong>{movie.title}</strong></label>
-                            </div>
-                            <div>
-                                <label>Director: <strong>{movie.director}</strong></label>
-                            </div>
-                            <div>
-                                <label>Genre: <strong>{movie.genre}</strong></label>
-                            </div>
-                            <div>
-                                <label>Metascore: <strong>{movie.metascore}</strong></label>
-                            </div>
-                            <div>
-                                <label>Description:</label>
-                                <p><strong>{movie.description}</strong></p>
-                            </div>
-                        </section>
-                        
-                        <section>
-                            <span className="m-2 btn btn-dark">Favorite</span>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
-                        </section>
+    return (
+        <div className="modal-page col">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h4 className="modal-title">{movie.title} Details</h4>
+                    </div>
+                    <div className="modal-body">
+                        <div className="flexContainer">
+                            <section className="movie-details">
+                                <div>
+                                    <label>
+                                        Title: <strong>{movie.title}</strong>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Director: <strong>{movie.director}</strong>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Genre: <strong>{movie.genre}</strong>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Metascore: <strong>{movie.metascore}</strong>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>Description:</label>
+                                    <p>
+                                        <strong>{movie.description}</strong>
+                                    </p>
+                                </div>
+                            </section>
+
+                            <section>
+                                <span className="m-2 btn btn-dark">Favorite</span>
+                                <span className="delete">
+                                    <input
+                                        type="button"
+                                        className="m-2 btn btn-danger"
+                                        value="Delete"
+                                        onClick={() => {
+                                            deleteMovie(movie.id);
+                                            push("/movies");
+                                        }}
+                                    />
+                                </span>
+                            </section>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>);
-}
+    );
+};
 
-export default Movie;
+const mapStateToProps = (state) => {
+    return {
+        movies: state.movies,
+    };
+};
+
+export default connect(mapStateToProps, { deleteMovie })(Movie);
