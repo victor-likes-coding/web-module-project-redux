ac3d1f856dc8d2acd32f2ddc04099e1f6cc4cd09
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-20 18:23:16 -0500

    add additional case in reducer

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,20 +1,26 @@
-import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
-import movies from './../data.js';
+import { ADD_MOVIE, DELETE_MOVIE } from "../actions/movieActions.js";
+import movies from "./../data.js";
 
 const initialState = {
     movies: movies,
-    appTitle: "IMDB Movie Database"
-}
+    appTitle: "IMDB Movie Database",
+};
 
 const reducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case DELETE_MOVIE:
             return {
-                movies: state.movies.filter(item=>(action.payload !== item.id))
-            }
+                movies: state.movies.filter((item) => action.payload !== item.id),
+            };
+
+        case ADD_MOVIE:
+            return {
+                movies: [...state.movies, action.payload],
+            };
+
         default:
             return state;
     }
-}
+};
 
 export default reducer;
