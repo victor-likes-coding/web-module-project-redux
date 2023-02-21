16b269d2aa30ed51c4d44b270d0fe2dd3e454076
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-20 19:44:54 -0500

    update to use connect give it state

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
+const FavoriteMovieList = ({ favorites, displayFavorites }) => {
+    return (
+        <div className="col-xs savedContainer">
+            <h5>Favorite Movies</h5>
+            {displayFavorites &&
+                favorites.map((movie) => {
+                    return (
+                        <div key={movie.id}>
+                            <Link
+                                className="btn btn-light savedButton"
+                                to={`/movies/${movie.id}`}>
+                                {movie.title}
+                                <span>
+                                    <span className="material-icons">remove_circle</span>
+                                </span>
+                            </Link>
+                        </div>
+                    );
+                })}
+        </div>
+    );
+};
 
-const FavoriteMovieList = (props) => {
-    const favorites = [];
-    
-    return (<div className="col-xs savedContainer">
-        <h5>Favorite Movies</h5>
-        {
-            favorites.map(movie=>{
-                return <div key={movie.id}>
-                    <Link className="btn btn-light savedButton" to={`/movies/${movie.id}`}>
-                        {movie.title}
-                        <span><span class="material-icons">remove_circle</span></span>
-                    </Link> 
-                </div>
-            })
-        }
-    </div>);
-}
+const mapStateToProps = ({ favorites }) => {
+    return {
+        favorites: favorites.favorites,
+        displayFavorites: favorites.displayFavorites,
+    };
+};
 
-
-export default FavoriteMovieList;
+export default connect(mapStateToProps, {})(FavoriteMovieList);
